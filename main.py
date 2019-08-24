83eb734cb32e62d964e7f4801be5d5180d8dc24e
Andrew K <29694231+AWK8923@users.noreply.github.com>
2019-08-24 17:25:20 -0400

    Made the program pick a random video from the /videos directory instead of just the first

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ import youtube_dl
 import cv2
 import time
 import conf
-
-inputfile = []
+import random
+#Grab all possible videos from /videos directory
+videosinput = []
 for root, dirs, files in os.walk(r'videos/'):
     for file in files:
-        inputfile.append('videos/' + file)
-print(inputfile[0])
+        videosinput.append('videos/' + file)
+print(videosinput)
+
+#Pick a video to be played
+currentshow = videosinput[random.randrange(0,len(videosinput))]
 
 #Video properties.
-vid = cv2.VideoCapture(inputfile[0])
+vid = cv2.VideoCapture(currentshow)
 height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
 width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
 fcount = vid.get(cv2.CAP_PROP_FRAME_COUNT)
@@ -33,7 +37,7 @@ logoy = height - pixelsoff - logosize
 print(f"FPS: {fps}\nFCOUNT: {fcount}\nLENGTH: {length}s ({length/60}m)")
 
 #Create mpv command.
-process = subprocess.Popen(f'mpv/mpv -fs --lavfi-complex="[vid2] scale={logosize}:{logosize},format=rgba,colorchannelmixer=aa={conf.logoopacity} [logo],[vid1][logo] overlay=x={logox}:y={logoy} [vo]" {inputfile[0]} --external-file={logoimage}',
+process = subprocess.Popen(f'mpv/mpv -fs --lavfi-complex="[vid2] scale={logosize}:{logosize},format=rgba,colorchannelmixer=aa={conf.logoopacity} [logo],[vid1][logo] overlay=x={logox}:y={logoy} [vo]" {currentshow} --external-file={logoimage}',
     stdout=subprocess.PIPE)
 out, err = process.communicate()
 
