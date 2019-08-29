80e7b65b47ded6741a86dea3a14125a4c965c9c9
DigiDuncan <digiduncan@gmail.com>
2019-08-29 18:41:38 -0400

    Re-refactor and work on loop.

--- conf.py
+++ conf.py
@@ -1,3 +1,5 @@
+#Name Options
+channelname = 'DDTV' #The name of your TV channel.
 #Logo Options
 logoimage = 'DDHQ.png' #image should be stored in /custom directory
 logoopacity = 0.5 #Sets the opacity level of the "TV station's logo". Set to 0 to hide logo
--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import conf
 import random
 import datetime
 
-#Logo properties from config.
+#Load values from config.
 logoimage = 'custom/' + conf.logoimage
 logofraction = 15 / conf.logosize #Logo size will be 1/logofraction of video width.
 insetfraction = conf.insetfraction.split("/")
 offsetdenominator = float(insetfraction[1]) #Logo will be set into the video by offsetnumerator/offsetdenominator pixels relative to video width.
 offsetnumerator = offsetdenominator - float(insetfraction[0])
 
+#Get the properties of a video passed to this function.
 def getVideoProperties(video):
     #Current video properties.
     vid = cv2.VideoCapture(video)
@@ -31,10 +32,35 @@ def getVideoProperties(video):
 
     return{"height": height, "width": width, "length": length, "logosize": logosize, "logox": logox, "logoy": logoy}
 
+#Play a video.
+def mpv(passedfile):
+    #Update tracker.
+    global currentfile
+    currentfile = passedfile
+
+    #Get video properties of current video.
+    vp = getVideoProperties(passedfile)
+    logosize, logox, logoy = vp["logosize"], vp["logox"], vp["logoy"]
+
+    #Create mpv command.
+    process = subprocess.Popen(f'mpv/mpv -fs -title="{conf.channelname}" -keep-open=yes --lavfi-complex="[vid2] scale={logosize}:{logosize},format=rgba,colorchannelmixer=aa={conf.logoopacity} [logo],[vid1][logo] overlay=x={logox}:y={logoy} [vo]" {passedfile} --external-file={logoimage}',
+    stdout=subprocess.PIPE)
+    out, err = process.communicate()
+
+    #Print mpv command results.
+    print(out.decode("utf-8"))
+    return(out.decode("utf-8"))
+
+#Constants.
 now = datetime.datetime.now()
-videosinput = []
-commercialsinput = []
+videosinput = commercialsinput = []
+showlengths = comlengths = {}
 print(now.minute)
+
+#Trackers.
+queueposition = 0
+commercialqueueposition = 0
+currentfile = ""
 
 #Grab all "TV shows"
 for root, dirs, files in os.walk(r'videos/'):
@@ -47,52 +73,21 @@ for root, dirs, files in os.walk(r'commercials/'):
         commercialsinput.append('commercials/' + file)
 
 #Calculate video lengths.
-showlengths = comlengths = {}
 for file in videosinput:
     showlengths.update({file: getVideoProperties(file)["length"]})
-
-print(showlengths)
 
 #Randomize video order
 random.shuffle(videosinput)
 random.shuffle(commercialsinput)
 
 #Pick a video to be played
-queueposition = 0
-commercialqueueposition = 0
 currentshow = videosinput[queueposition]
-
-#Play a video.
-def mpv(passedfile):
-    #Get video properties of current video.
-    vp = getVideoProperties(passedfile)
-    logosize, logox, logoy = vp["logosize"], vp["logox"], vp["logoy"]
-
-    #Create mpv command.
-    process = subprocess.Popen(f'mpv/mpv -fs -track-auto-selection= --lavfi-complex="[vid2] scale={logosize}:{logosize},format=rgba,colorchannelmixer=aa={conf.logoopacity} [logo],[vid1][logo] overlay=x={logox}:y={logoy} [vo]" {passedfile} --external-file={logoimage}',
-    stdout=subprocess.PIPE)
-    out, err = process.communicate()
-
-    #Print mpv command results.
-    print(out.decode("utf-8"))
-    return(out.decode("utf-8"))
-
-#Play the first video, and see if it exited on it's own.
-output = mpv(currentshow)
 
 #Exit code handling.
 while True:
-    if output.endswith("Exiting... (End of file)\r\n"):
-        if now.minute >= 25 and now.minute <= 35:
-            commercialqueueposition = commercialqueueposition + 1
-            currentshow = commercialsinput[commercialqueueposition]
-            mpv(currentshow)
-        if now.minute >= 55 or now.minute <= 5:
-            commercialqueueposition = commercialqueueposition + 1
-            currentshow = commercialsinput[commercialqueueposition]
-            mpv(currentshow)
-        queueposition = queueposition + 1
-        currentshow = videosinput[queueposition]
-        mpv(currentshow)
-    if output.endswith("Exiting... (Quit)"):
-        os.system("taskkill /IM mpv.exe /F")
+    print("Loop!")
+    #Update time.
+    now = datetime.datetime.now()
+    #Play the current video.
+    mpv(currentshow)
+    print(getVideoProperties(currentfile)["length"])
